91e40b4d246337608455ff69828365d90b40488c
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-08-29 21:17:28 +0300

    created level manager for future use

---
+++ modules/Levels/LevelManager.js
@@ -0,0 +1,11 @@
+class LevelManager {
+  static async SetCurrentLevel(context, level) {
+    await level.city.awake();
+    context.currentLevel = level;
+  }
+
+  static ChangeLevel(currentLevel, nextLevel) {
+    // DO OTHER OPERATIONS LIKE SETTIG GAME STATE AND STUFF HERE, =)
+    currentLevel.end();
+  }
+}
