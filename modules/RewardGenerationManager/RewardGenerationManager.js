c75cdf199c8e75d614cb7384dc635cde376a2313
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-10-10 20:52:31 +0300

    update to reward placement logic

--- modules/RewardGenerationManager/RewardGenerationManager.js
+++ modules/RewardGenerationManager/RewardGenerationManager.js
@@ -6,45 +6,41 @@ class RewardGenerationManagement {
 		this.context = context;
 		this.scene = this.context.gameWorld.scene;
 
-		this.modelLength = 37;
-
 		this.delta = new THREE.Clock();
 
 		this.coinPositionsX = [-2.5, 0, 2.5];
 		this.coinPositionsY = [1, 2.25];
+
+		this.spawnPosition = new THREE.Vector3(0, 0, 2000);
+		this.placementPostion = new THREE.Vector3(0, 0, 0);
+
 		this.init();
 	}
 
 	init() {
 		this.coinindex = 0;
 
-		this.totalCoins = 100;
+		this.totalCoins = 20;
 		this.coins = [];
 
+		let coin = new Coin(this.context, this.spawnPosition);
+
 		for (let index = 0; index < this.totalCoins; index++) {
-			this.coins[index] = new Coin(this.context, new THREE.Vector3(0, 0, -999));
+			this.coins.push(coin.clone());
 		}
 	}
 
-	placeReward(z, meshToPlace) {
-		let placementPostion = new THREE.Vector3(
-			this.coinPositionsX[UTIL.randomIntFromInterval(0, 2)],
-			this.coinPositionsY[UTIL.randomIntFromInterval(0, 1)],
-			z
-		);
+	placeReward(z) {
+		(this.placementPostion.x =
+			this.coinPositionsX[UTIL.randomIntFromInterval(0, 2)]),
+			(this.placementPostion.y =
+				this.coinPositionsY[UTIL.randomIntFromInterval(0, 1)]),
+			(this.placementPostion.z = z);
 
-		// console.log(placementPostion);
 		let reward = this.coins[this.coinindex % this.totalCoins];
-		// console.log("CURRENT COIN INDEX IS :" + (this.coinindex % this.totalCoins));
-		reward.updatePosition(placementPostion);
+		reward.updatePosition(this.placementPostion);
 		this.coinindex++;
 		reward = reward.coinMesh;
-		// reward.position.copy(cannonToThreeVec3(placementPostion));
-		// reward.collider.position = placementPostion;
-		// console.log(reward);
-
-		this.scene.add(reward);
-		// meshToPlace.add(reward);
 	}
 
 	update() {}
