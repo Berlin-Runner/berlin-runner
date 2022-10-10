296a6027e0962fe3e79836266795a6935abe28cd
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-10-10 20:51:21 +0300

    moving enemy added

--- modules/ObstacleGenerationManager/ObstacleGenerationManager.js
+++ modules/ObstacleGenerationManager/ObstacleGenerationManager.js
@@ -1,6 +1,8 @@
 import { Vec3 } from "../../libs/cannon-es.js";
 import { UTIL } from "../Util/UTIL.js";
+import { Kermit } from "./Kermit/Kermit.js";
 import { MetalBarrier } from "./MetalBarrier/MetalBarrier.js";
+import { Ramp } from "./Ramp/Ramp.js";
 
 class ObstacleGenerationManager {
 	constructor(context) {
@@ -17,32 +19,48 @@ class ObstacleGenerationManager {
 		this.obstacleIndex = 0;
 		this.totalObstacles = 20;
 		this.obstacles = [];
+		this.ramps = [];
+		this.kermits = [];
 
 		// this.metalBarrier = new MetalBarrier(this.context);
 
 		let initialSpawnPosition = new THREE.Vector3(0, 0, -2000);
+		let ramp = new Ramp(this.context, initialSpawnPosition);
+		let kermit = new Kermit(this.context, initialSpawnPosition);
 
 		for (let index = 0; index < this.totalObstacles; index++) {
 			this.obstacles[index] = new MetalBarrier(
 				this.context,
 				initialSpawnPosition
 			);
+
+			this.ramps[index] = ramp.clone();
+
+			this.kermits.push(kermit.clone());
 		}
 	}
 
 	placeObstacles(zPos) {
 		let placementPosition = new THREE.Vector3(
-			this.obstaclePositionX[UTIL.randomIntFromInterval[(0, 2)]],
+			this.obstaclePositionX[UTIL.randomIntFromInterval(0, 2)],
 			0,
 			zPos
 		);
 
 		let obstacle = this.obstacles[this.obstacleIndex % this.totalObstacles];
 		obstacle.updatePosition(placementPosition);
-		this.obstacleIndex++;
 
 		obstacle = obstacle.obstacleMesh;
-		// this.scene.add(obstacle);
+
+		placementPosition.z += 30;
+		let slantRamp = this.ramps[this.obstacleIndex % this.totalObstacles];
+		slantRamp.updatePosition(placementPosition);
+
+		placementPosition.z += 38;
+		let kermitBoy = this.kermits[this.obstacleIndex % this.totalObstacles];
+		kermitBoy.updatePosition(placementPosition);
+
+		this.obstacleIndex++;
 	}
 }
 
