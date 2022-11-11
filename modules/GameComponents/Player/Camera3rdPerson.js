14a84cab179fefd5547cefb41243d276084546e5
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-11-11 16:20:43 +0300

    implemented camera offset profile, =)

--- modules/GameComponents/Player/Camera3rdPerson.js
+++ modules/GameComponents/Player/Camera3rdPerson.js
@@ -5,12 +5,47 @@ class Camer3rdPerson {
 		this.player = player;
 
 		this.time = new THREE.Clock();
+
+		this.cameraOffsetProfiles = [
+			{
+				//BIRDS_EYE_VIEW
+				y: 4.75,
+				z: 8.5,
+			},
+			{
+				//NORMAL_VIEW
+				y: 2,
+				z: 5.5,
+			},
+			{
+				//COACH_VIEW
+			},
+		];
+
+		this.currentCameraOffsetProfile = this.cameraOffsetProfiles[1];
+
+		this.addClassSettings();
 	}
 
 	update() {
 		if (!this.camera || !this.player) return;
-		this.camera.position.y = this.player.position.y + 4.75;
-		this.camera.position.z = this.player.position.z + 8.5;
+		this.camera.position.y =
+			this.player.position.y + this.currentCameraOffsetProfile.y;
+		this.camera.position.z =
+			this.player.position.z + this.currentCameraOffsetProfile.z;
+	}
+
+	addClassSettings() {
+		let classSettings = this.context.gui.addFolder("CAMERA OFFSET PROFILE");
+		classSettings
+			.add(this, "currentCameraOffsetProfile", {
+				BIRDS_EYE_VIEW: 0,
+				NORMAL_VIEW: 1,
+			})
+			.onChange((value) => {
+				console.log(value);
+				this.currentCameraOffsetProfile = this.cameraOffsetProfiles[value];
+			});
 	}
 }
 
