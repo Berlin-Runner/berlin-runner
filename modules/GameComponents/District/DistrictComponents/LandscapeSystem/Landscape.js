5aaab6d3f6df6e772d7c2a45a9eb6713c62f678d
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-11-11 11:52:55 +0300

    updated with first tile

--- modules/GameComponents/District/DistrictComponents/LandscapeSystem/Landscape.js
+++ modules/GameComponents/District/DistrictComponents/LandscapeSystem/Landscape.js
@@ -6,24 +6,24 @@ class Landscape {
 	async loadLandscape(url) {
 		let { model } = await UTIL.loadModel(url);
 
-		return model.children[0];
+		return model;
 	}
 
 	async initLandscape(url) {
 		let model = await this.loadLandscape(url);
-		let lanscapeMap = model.material.map;
+		// let lanscapeMap = model.material.map;
 
-		model.material = THREE.extendMaterial(THREE.MeshStandardMaterial, {
-			class: THREE.CustomMaterial,
+		// model.material = THREE.extendMaterial(THREE.MeshStandardMaterial, {
+		// 	class: THREE.CustomMaterial,
 
-			vertex: {
-				transformEnd: UTIL.getFoldableShader(),
-			},
-		});
+		// 	vertex: {
+		// 		transformEnd: UTIL.getFoldableShader(),
+		// 	},
+		// });
 
-		model.material.uniforms.map.value = lanscapeMap;
+		// model.material.uniforms.map.value = lanscapeMap;
 
-		model.material.map = lanscapeMap;
+		// model.material.map = lanscapeMap;
 
 		return model;
 	}
