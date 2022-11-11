ce3fc4d4aeead60ce643819c50cc27102477c163
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2022-11-11 17:24:57 +0300

    implemented a 3D logo scene

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ import { AudioManager } from "./modules/Core/AudioManager/AudioManager.js";
 import { LevelZero } from "./modules/GameComponents/Levels/LevelZero/LevelZero.js";
 import { UIManager } from "./modules/GameComponents/UIManager/UIManager.js";
 
+// import { GLTFLoader } from "/libs/GLTFLoader.js";
+
 import * as dat from "/libs/dat.gui.module.js";
 
 class Game {
@@ -149,3 +151,73 @@ class Game {
 
 let gameInstance = new Game();
 requestAnimationFrame(gameInstance.animate.bind(gameInstance));
+
+class BerlinRunnerTextLogo {
+	constructor() {
+		this.canvas = document.getElementById("game-title");
+		this.camera = new THREE.PerspectiveCamera(
+			60,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			40
+		);
+
+		this.tf = 1;
+
+		this.camera.position.z = 4;
+
+		this.scene = new THREE.Scene();
+		// this.scene.background = new THREE.Color("blue");
+
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			powerPreference: "high-performance",
+		});
+
+		this.renderer.setSize(350, 150);
+		this.renderer.setPixelRatio(window.devicePixelRatio * 0.5);
+
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		this.canvas.appendChild(this.renderer.domElement);
+
+		this.controls = new THREE.OrbitControls(
+			this.camera,
+			this.renderer.domElement
+		);
+		this.controls.enabled = false;
+
+		let ambLight = new THREE.AmbientLight("#ddd", 0.75);
+		this.scene.add(ambLight);
+
+		let sunLight = new THREE.DirectionalLight("#fff", 1);
+		sunLight.position.set(0, 1.5, 1);
+		this.scene.add(sunLight);
+
+		this.loadLogo();
+	}
+
+	loadLogo() {
+		let loader = new THREE.GLTFLoader();
+		loader.load("/assets/models/berlin-runner-text.glb", (res) => {
+			this.logoModel = res.scene.children[0];
+			this.scene.add(this.logoModel);
+			console.log(this.logoModel);
+		});
+	}
+
+	animate() {
+		this.tf += 0.1;
+		requestAnimationFrame(this.animate.bind(this));
+		console.log("hh");
+		this.controls.update();
+		this.renderer.render(this.scene, this.camera);
+
+		if (this.logoModel) {
+			this.logoModel.rotation.y = 0.25 * Math.sin(0.25 * this.tf);
+			this.logoModel.rotation.x = 0.25 * Math.sin(0.25 * this.tf);
+		}
+	}
+}
+
+let berlinRunnerLogo = new BerlinRunnerTextLogo();
+requestAnimationFrame(berlinRunnerLogo.animate.bind(berlinRunnerLogo));
