518aeaef771ddee83fc093b2c50c7937d0a87a3d
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2023-03-03 17:57:41 +0300

    folder restructuring

---
+++ modules/GameComponents/District/DistrictComponents/_/Stage/Stage.js
@@ -0,0 +1,45 @@
+import { Obstacle } from "../Obstacle.js";
+
+export default class Stage extends Obstacle {
+	constructor(context) {
+		super(context);
+		this.spawnPosition = new THREE.Vector3(0, 0, 0);
+		this.stateManager = this.context.gameStateManager;
+		this.stateBus = this.context.gameStateEventBus;
+
+		this.init();
+	}
+
+	init() {
+		this.scene = this.context.gameWorld.scene;
+
+		this.modelUrl =
+			"/modules/GameComponents/District/DistrictComponents/LandscapeSystem/LandscapeComponents/ObstacleGenerationManager/Stage/Model/starting-stage.glb";
+
+		this.loadStage();
+
+		this.setupEventSubscriber();
+	}
+
+	async loadStage() {
+		let model = this.initObstacle(this.modelUrl);
+
+		model.then((res) => {
+			this.stageMesh = res.model;
+			this.stageMesh.position.copy(this.spawnPosition);
+			this.context.startingStage = this.stageMesh;
+			this.context.startingStage.visible = false;
+			this.scene.add(this.stageMesh);
+		});
+	}
+
+	setupEventSubscriber() {
+		this.stateBus.subscribe("", () => {});
+	}
+
+	setupEventListners() {}
+
+	clone() {
+		return new Bus(this.context, this.spawnPosition);
+	}
+}
