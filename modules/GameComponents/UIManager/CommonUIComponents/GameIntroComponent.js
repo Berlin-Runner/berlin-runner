c98fff292abd5efb57003dac3864c0220001cc88
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2023-04-12 11:23:42 +0300

    picker order refactor

--- modules/GameComponents/UIManager/CommonUIComponents/GameIntroComponent.js
+++ modules/GameComponents/UIManager/CommonUIComponents/GameIntroComponent.js
@@ -1,11 +1,28 @@
 import { BaseUIComponent } from "../BaseUIComponent.js";
+import { LevelZero } from "../../Levels/LevelZero/LevelZero.js";
 class GameIntroComponent extends BaseUIComponent {
 	constructor(id, context) {
 		super(id, context);
 
+		this.camera = this.context.gameWorld.camera;
+
 		this.startGameButton = document.getElementById("start-game-button");
 		this.setUpComponentEventListners();
 		this.setupEventBusSubscriptions();
+
+		this.init();
+	}
+
+	init() {
+		this.tiles = [
+			this.context.tileOne,
+			this.context.tileTwo,
+			this.context.tileThree,
+			this.context.tileFour,
+			this.context.tileFive,
+			this.context.tileSix,
+			this.context.tileSeven,
+		];
 	}
 
 	setUpComponentEventListners() {
@@ -32,7 +49,30 @@ class GameIntroComponent extends BaseUIComponent {
 	startGame() {
 		this.hideComponent();
 
-		this.stateManager.showDistrictPicker();
+		gsap.to(this.camera.position, {
+			x: 0,
+			y: 50,
+			z: -110,
+			duration: 0,
+		});
+		gsap.to(this.camera.rotation, { x: 0, y: 0, z: 0, duration: 0.75 });
+		this.stateBus.publish("display-chracter-selector");
+		this.context.gameStateManager.showCharacterPicker();
+		this.initLevels();
+	}
+
+	initLevels() {
+		this.tiles.forEach((tile, index) => {
+			tile.position.z = 0;
+			tile.position.x = 0;
+			tile.position.y = -0.1;
+			tile.scale.setScalar(1);
+			tile.rotation.set(0, 0, 0);
+		});
+		this.context.levelZero = new LevelZero(this.context);
+		this.context.levelZero.activeLevel = true;
+
+		this.context.currentLevel = this.context.levelZero;
 	}
 
 	muteToggle() {
