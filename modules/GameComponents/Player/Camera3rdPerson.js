43ffc59ef4357bf31206cc4049328713b53dfc4b
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2023-04-27 11:27:34 +0300

    updated camera follow logic

--- modules/GameComponents/Player/Camera3rdPerson.js
+++ modules/GameComponents/Player/Camera3rdPerson.js
@@ -1,57 +1,61 @@
-class Camer3rdPerson {
+class Camera3rdPerson {
 	constructor(context, player) {
 		this.context = context;
 		this.camera = this.context.gameWorld.camera;
 		this.player = player;
 
-		this.time = new THREE.Clock();
+		this.follow = true;
 
-		this.follow = false;
+		this.cameraOffsetProfiles = {
+			BIRDS_EYE_VIEW: { y: 4.75, z: 8.5 },
+			NORMAL_VIEW: { y: 2, z: 5.5 },
+			COACH_VIEW: {},
+		};
 
-		this.cameraOffsetProfiles = [
-			{
-				//BIRDS_EYE_VIEW
-				y: 4.75,
-				z: 8.5,
-			},
-			{
-				//NORMAL_VIEW
-				y: 2,
-				z: 5.5,
-			},
-			{
-				//COACH_VIEW
-			},
-		];
+		this.currentProfile = "NORMAL_VIEW";
+		this.currentCameraOffset = this.cameraOffsetProfiles[this.currentProfile];
 
-		this.currentIndex = 1;
-		this.currentCameraOffsetProfile = this.cameraOffsetProfiles[1];
+		this.cameraDistance = 5.5;
+		this.elevationAngle = Math.PI / 2.85;
+		this.rotationAngle = Math.PI / 2;
 
 		this.addClassSettings();
 	}
 
+	/*
+	 * With this approach, you can adjust the cameraDistance, elevationAngle, and rotationAngle properties to set the camera's position relative to the player. The update method will then compute the camera's position in Cartesian coordinates and set the camera to look at the player.
+	 */
 	update() {
 		if (!this.camera || !this.player || !this.follow) return;
-		this.camera.position.y =
-			this.player.position.y + this.currentCameraOffsetProfile.y;
-		this.camera.position.z =
-			this.player.position.z + this.currentCameraOffsetProfile.z;
+
+		const x =
+			this.cameraDistance *
+			Math.sin(this.elevationAngle) *
+			Math.cos(this.rotationAngle);
+		const y = this.cameraDistance * Math.cos(this.elevationAngle);
+		const z =
+			this.cameraDistance *
+			Math.sin(this.elevationAngle) *
+			Math.sin(this.rotationAngle);
+
+		this.camera.position.set(
+			this.player.position.x + x,
+			this.player.position.y + y,
+			this.player.position.z + z
+		);
+		this.camera.lookAt(this.player.position);
 	}
 
 	addClassSettings() {
-		let classSettings = this.context.gui.addFolder("CAMERA OFFSET PROFILE");
+		const classSettings = this.context.gui.addFolder("CAMERA OFFSET PROFILE");
 		classSettings
-			.add(this, "currentIndex", {
-				BIRDS_EYE_VIEW: 0,
-				NORMAL_VIEW: 1,
-			})
+			.add(this, "currentProfile", Object.keys(this.cameraOffsetProfiles))
 			.onChange((value) => {
-				console.log(value);
-				this.currentIndex = value;
-				this.currentCameraOffsetProfile = this.cameraOffsetProfiles[value];
+				this.currentProfile = value;
+				this.currentCameraOffset = this.cameraOffsetProfiles[value];
 			})
 			.name("CURRENT.PROFILE");
 	}
 }
 
-export { Camer3rdPerson };
+export { Camera3rdPerson };
