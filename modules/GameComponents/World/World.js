0abba2daf1ee283d3caecb34d1212d7fc61345ff
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2024-01-29 10:41:10 +0300

    added a rudimentray rain system

--- modules/GameComponents/World/World.js
+++ modules/GameComponents/World/World.js
@@ -17,6 +17,56 @@ class World_ {
 		this.initControls()
 
 		this.initLights()
+
+		// this.addSun()
+
+		this.addRain()
+
+		this.globalUniforms = {
+			time: { value: 0 },
+			globalBloom: { value: 0 },
+			noise: { value: null },
+		}
+
+		this.time = new THREE.Clock()
+		// console.log(this.time)
+
+		this.noise = `
+		float N21 (vec2 st) { // https://thebookofshaders.com/10/
+			return fract( sin( dot( st.xy, vec2(12.9898,78.233 ) ) ) *  43758.5453123);
+		}
+
+		float smoothNoise( vec2 ip ){ // https://www.youtube.com/watch?v=zXsWftRdsvU
+
+			vec2 lv = fract( ip );
+		  vec2 id = floor( ip );
+
+		  lv = lv * lv * ( 3. - 2. * lv );
+
+		  float bl = N21( id );
+		  float br = N21( id + vec2( 1, 0 ));
+		  float b = mix( bl, br, lv.x );
+
+		  float tl = N21( id + vec2( 0, 1 ));
+		  float tr = N21( id + vec2( 1, 1 ));
+		  float t = mix( tl, tr, lv.x );
+
+		  return clamp(mix( b, t, lv.y ) * 0.5 + 0.5, 0., 1.);
+		}
+
+		float smoothNoise2(vec2 p){
+
+		  p.y += time;
+		  p /= 4.;
+
+		  float n = smoothNoise(p) * 1.5;
+		  n += smoothNoise(p * 2.01) * 0.25;
+		  n += smoothNoise(p * 4.02) * 0.125;
+		  n += smoothNoise(p * 8.03) * 0.0625;
+		  n /= (1.5 + 0.25 + 0.125 + 0.0625);
+		  return clamp(n, 0., 1.);
+		}
+		`
 	}
 
 	initCamera() {
@@ -65,7 +115,6 @@ class World_ {
 	initLights() {
 		let ambLight = new THREE.AmbientLight("#ffffff", 1)
 		this.scene.add(ambLight)
-		this.addSun()
 	}
 
 	addSun() {
@@ -97,14 +146,14 @@ class World_ {
 		this.sunlight.shadow.camera.near = 0.5 // Default is 0.5
 		this.sunlight.shadow.camera.far = 500 // Default is 500
 
-		this.scene.add(this.sunlight)
+		// this.scene.add(this.sunlight)
 
-		this.animateSun()
+		// this.animateSun()
 	}
 
 	animateSun() {
 		// 60sec * [n]Minutes
-		let morningToMiddayDuration = 60 * 2
+		let morningToMiddayDuration = 60 * 0.5
 
 		const startPos = { x: 0, y: -2, z: -100 }
 		const endPos = { x: 0, y: 20, z: -15 }
@@ -146,6 +195,84 @@ class World_ {
 		})
 	}
 
+	addRain() {
+		// "drops"
+		let gPos = []
+		let gEnds = []
+		let gCount = 5000
+		for (let i = 0; i < gCount; i++) {
+			let x = THREE.MathUtils.randFloatSpread(15)
+			let y = THREE.MathUtils.randFloat(-5, 20)
+			let z = THREE.MathUtils.randFloatSpread(50)
+			let len = THREE.MathUtils.randFloat(0.25, 0.35)
+			gPos.push(x, y, z, x, y, z)
+			gEnds.push(0, len, 1, len)
+		}
+		let gg = new THREE.BufferGeometry()
+		gg.setAttribute("position", new THREE.Float32BufferAttribute(gPos, 3))
+		gg.setAttribute("gEnds", new THREE.Float32BufferAttribute(gEnds, 2))
+		let gm = new THREE.LineBasicMaterial({
+			color: 0xffffff,
+			transparent: true,
+			onBeforeCompile: (shader) => {
+				shader.uniforms.time = this.globalUniforms.time
+				shader.uniforms.noiseTex = this.globalUniforms.noise
+				shader.uniforms.globalBloom = this.globalUniforms.globalBloom
+				shader.vertexShader = `
+      uniform float time;
+      uniform sampler2D noiseTex;
+      attribute vec2 gEnds;
+      varying float vGEnds;
+      varying float vH;
+
+      ${shader.vertexShader}
+    `.replace(
+					`#include <begin_vertex>`,
+					`#include <begin_vertex>
+
+	vec3 pos = position;
+
+	vec2 nUv = (vec2(pos.x, -pos.z) - vec2(-25.)) / 50.;
+	float h = texture2D(noiseTex, nUv).g;
+	h = (h - 0.5) * 5.;
+
+	pos.y = -mod(10. - (pos.y - time * 7.5), 15.) + 5.;
+	h = pos.y - h;
+	pos.y += gEnds.x * gEnds.y;
+	transformed = pos;
+	vGEnds = gEnds.x;
+	vH = smoothstep(3., 0., h);
+	`
+				)
+
+				shader.fragmentShader = `
+	uniform float time;
+	uniform float globalBloom;
+	varying float vGEnds;
+	varying float vH;
+	${this.noise}
+	${shader.fragmentShader}
+	`.replace(
+					`vec4 diffuseColor = vec4( diffuse, opacity );`,
+					`
+	float op = 1. - vGEnds;
+	op = pow(op, 3.);
+	float h = (pow(vH, 3.) * .5 + 0.5);
+	vec3 col = diffuse * h; // lighter close to the surface
+	col *= 1. + smoothstep(0.99, 1., h); // sparkle at the surface
+	if (globalBloom > 0.5) {
+	//col *= 0.5;
+	}
+	vec4 diffuseColor = vec4( col, op );
+
+	`
+				)
+			},
+		})
+		let go = new THREE.LineSegments(gg, gm)
+		this.scene.add(go)
+	}
+
 	setupResize() {
 		window.addEventListener("resize", this.resize.bind(this))
 	}
@@ -162,6 +289,19 @@ class World_ {
 	update() {
 		if (this.controls.enabled) this.controls.update()
 		this.renderer.render(this.scene, this.camera)
+		this.updateScene()
+	}
+
+	updateScene(t) {
+		// for(let i = 0; i<instCount; i++){
+		//   let li = lucesInit[i];
+		//   let z = ((li.y + t + 25) % 50) - 25;
+		//   luces[i].y = z;
+		//   luces[i].w = (Math.sin(t * li.w * (i % 3 + 1)) * Math.cos(t  * li.w * (i % 5 + 1)) * 0.25 + 0.25) * li.z + li.z * 0.75;
+		//   lg.attributes.instData.setY(i, z);
+		// }
+		// lg.attributes.instData.needsUpdate = true
+		this.globalUniforms.time.value = this.time.getElapsedTime()
 	}
 
 	addClassSettings() {
