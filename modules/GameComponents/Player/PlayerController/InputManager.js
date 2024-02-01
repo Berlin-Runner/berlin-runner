9c3b3bd0475570c4747b8115a48a2b602ad7756e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2024-02-01 10:45:14 +0300

    jumping and sliding bug fixed

--- modules/GameComponents/Player/PlayerController/InputManager.js
+++ modules/GameComponents/Player/PlayerController/InputManager.js
@@ -1,117 +1,125 @@
-import MovementManager from "./MovementManager.js";
-import AnimationManager from "./AnimationManager.js";
+import MovementManager from "./MovementManager.js"
+import AnimationManager from "./AnimationManager.js"
 
 const KEY_CODES = {
 	LEFT: ["ArrowLeft", "KeyA"],
-	RIGHT: ["ArrowRight","KeyD"],
-	JUMP: ["ArrowUp","KeyW", "Space"],
-	SLIDE: ["ArrowDown","KeyS"],
-};
+	RIGHT: ["ArrowRight", "KeyD"],
+	JUMP: ["ArrowUp", "KeyW", "Space"],
+	SLIDE: ["ArrowDown", "KeyS"],
+}
 
 const LANES = {
 	CENTER: "center",
 	LEFT: "left",
 	RIGHT: "right",
-};
+}
 
 export default class InputManager {
 	constructor(context, player) {
-		this.context = context;
-		this.player = player;
-		this.movementManager = new MovementManager(this.context, this.player);
-		this.animationManager = new AnimationManager(this.context);
+		this.context = context
+		this.player = player
+		this.movementManager = new MovementManager(this.context, this.player)
+		this.animationManager = new AnimationManager(this.context)
 
 		this.keyStates = {
 			left: false,
 			right: false,
 			jump: false,
 			slide: false,
-		};
+		}
 
-		this.initKeyboardListeners();
-		this.initSwipeListeners();
+		this.initKeyboardListeners()
+		this.initSwipeListeners()
 	}
 
 	left() {
-		this.keyStates.left = true;
-		this.movementManager.moveToLane(LANES.LEFT);
+		this.keyStates.left = true
+		this.movementManager.moveToLane(LANES.LEFT)
 	}
 	right() {
-		this.keyStates.right = true;
-		this.movementManager.moveToLane(LANES.RIGHT);
+		this.keyStates.right = true
+		this.movementManager.moveToLane(LANES.RIGHT)
 	}
 	up() {
-		this.keyStates.jump = true;
-		this.movementManager.jump();
-		this.animationManager.playJumpAnimation();
+		if (this.context.G.PLAYER_SLIDING) return
+		this.keyStates.jump = true
+		this.movementManager.jump()
+		this.animationManager.playJumpAnimation()
 	}
 	down() {
-		this.keyStates.slide = true;
-		this.movementManager.slide();
-		this.animationManager.playSlideAnimation();
+		if (this.context.G.PLAYER_JUMPING) return
+		this.keyStates.slide = true
+		this.movementManager.slide()
+		this.animationManager.playSlideAnimation()
 	}
 
 	initKeyboardListeners() {
 		document.addEventListener("keydown", (event) => {
 			if (KEY_CODES.LEFT.includes(event.code) && !this.keyStates.left) {
-				this.left();
-			} else if (KEY_CODES.RIGHT.includes(event.code) && !this.keyStates.right) {
-				this.right();
+				this.left()
+			} else if (
+				KEY_CODES.RIGHT.includes(event.code) &&
+				!this.keyStates.right
+			) {
+				this.right()
 			} else if (KEY_CODES.JUMP.includes(event.code) && !this.keyStates.jump) {
-				this.up();
-			} else if (KEY_CODES.SLIDE.includes(event.code) && !this.keyStates.slide) {
-				this.down();
+				this.up()
+			} else if (
+				KEY_CODES.SLIDE.includes(event.code) &&
+				!this.keyStates.slide
+			) {
+				this.down()
 			}
-		});
+		})
 
 		document.addEventListener("keyup", (event) => {
 			if (KEY_CODES.LEFT.includes(event.code)) {
-				this.keyStates.left = false;
+				this.keyStates.left = false
 			} else if (KEY_CODES.RIGHT.includes(event.code)) {
-				this.keyStates.right = false;
+				this.keyStates.right = false
 			} else if (KEY_CODES.JUMP.includes(event.code)) {
-				this.keyStates.jump = false;
+				this.keyStates.jump = false
 			} else if (KEY_CODES.SLIDE.includes(event.code)) {
-				this.keyStates.slide = false;
+				this.keyStates.slide = false
 			}
-		});
+		})
 	}
 
 	initSwipeListeners() {
-		const hammertime = new Hammer(document.getElementById("webgl"), {});
+		const hammertime = new Hammer(document.getElementById("webgl"), {})
 
-		hammertime.get("swipe").set({ direction: Hammer.DIRECTION_ALL });
+		hammertime.get("swipe").set({ direction: Hammer.DIRECTION_ALL })
 
 		hammertime.on("swipeleft", (e) => {
-			this.left();
+			this.left()
 			setTimeout(() => {
-				this.keyStates.left = false;
-			}, 100);
-		});
+				this.keyStates.left = false
+			}, 100)
+		})
 
 		hammertime.on("swiperight", (e) => {
-			this.right();
+			this.right()
 			setTimeout(() => {
-				this.keyStates.right = false;
-			}, 100);
-		});
+				this.keyStates.right = false
+			}, 100)
+		})
 
 		hammertime.on("swipedown", (e) => {
-			this.down();
+			this.down()
 			setTimeout(() => {
-				this.keyStates.slide = false;
-			}, 100);
-		});
+				this.keyStates.slide = false
+			}, 100)
+		})
 
 		hammertime.on("swipeup", (e) => {
-			this.up();
+			this.up()
 			setTimeout(() => {
-				this.keyStates.jump = false;
-			}, 100);
-		});
+				this.keyStates.jump = false
+			}, 100)
+		})
 	}
 
 	isKeyDown(key) {
-		return this.keysDown[key];
+		return this.keysDown[key]
 	}
 }
--- modules/GameComponents/Player/PlayerController/MovementManager.js
+++ modules/GameComponents/Player/PlayerController/MovementManager.js
@@ -2,22 +2,22 @@ const LANES = {
 	CENTER: "center",
 	LEFT: "left",
 	RIGHT: "right",
-};
+}
 export default class MovementManager {
 	constructor(context, player) {
-		this.context = context;
-		this.player = player;
-		this.camera = this.context.gameWorld.camera;
-		this._init();
+		this.context = context
+		this.player = player
+		this.camera = this.context.gameWorld.camera
+		this._init()
 	}
 
 	_init() {
-		this.movementEventBus = this.context.playerMovementEventBus;
+		this.movementEventBus = this.context.playerMovementEventBus
 
-		this.tweenDuration = 0.95;
+		this.tweenDuration = 0.95
 
-		this.currentLane = LANES.CENTER;
-		this.moveToLane(LANES.CENTER);
+		this.currentLane = LANES.CENTER
+		this.moveToLane(LANES.CENTER)
 	}
 
 	moveObjectToPosition(object, position, duration, finishCallBack) {
@@ -26,9 +26,9 @@ export default class MovementManager {
 			duration,
 			ease: "power4.out",
 			onComplete: () => {
-				setTimeout(finishCallBack, 500);
+				setTimeout(finishCallBack, 500)
 			},
-		});
+		})
 	}
 
 	rotateObject(object, angle) {
@@ -40,104 +40,109 @@ export default class MovementManager {
 				gsap.to(object.rotation, {
 					y: Math.PI,
 					duration: this.tweenDuration / 2,
-				});
+				})
 			},
-		});
+		})
 	}
 
 	moveToCenter() {
-		this.moveObjectToPosition(this.player, 0, this.tweenDuration, () => {});
-		this.moveObjectToPosition(this.camera, 0, this.tweenDuration * 0.75);
+		this.moveObjectToPosition(this.player, 0, this.tweenDuration, () => {})
+		this.moveObjectToPosition(this.camera, 0, this.tweenDuration * 0.75)
 		if (this.prevLane == LANES.LEFT)
-			this.rotateObject(this.player, Math.PI * 0.75);
+			this.rotateObject(this.player, Math.PI * 0.75)
 		if (this.prevLane == LANES.RIGHT)
-			this.rotateObject(this.player, Math.PI * 1.25);
+			this.rotateObject(this.player, Math.PI * 1.25)
 	}
 
 	pullToCenter() {
-		this.moveObjectToPosition(this.player, 0, 1);
+		this.moveObjectToPosition(this.player, 0, 1)
 
 		if (this.currentPlayerLane == this.lanes.left)
-			this.rotateObject(this.player, Math.PI * 0.75);
+			this.rotateObject(this.player, Math.PI * 0.75)
 		if (this.currentPlayerLane == this.lanes.right)
-			this.rotateObject(this.player, Math.PI * 1.25);
-		this.currentPlayerLane = this.lanes.center;
+			this.rotateObject(this.player, Math.PI * 1.25)
+		this.currentPlayerLane = this.lanes.center
 	}
 
 	moveLeft() {
-		this.movementEventBus.publish("player-side-moved");
-		this.rotateObject(this.player, Math.PI * 1.25);
-		this.moveObjectToPosition(this.camera, -2, this.tweenDuration * 0.75);
-		this.moveObjectToPosition(this.player, -2.5, this.tweenDuration, () => {});
+		this.movementEventBus.publish("player-side-moved")
+		this.rotateObject(this.player, Math.PI * 1.25)
+		this.moveObjectToPosition(this.camera, -2, this.tweenDuration * 0.75)
+		this.moveObjectToPosition(this.player, -2.5, this.tweenDuration, () => {})
 	}
 
 	moveRight() {
-		this.movementEventBus.publish("player-side-moved");
-		this.rotateObject(this.player, Math.PI * 0.75);
-		this.moveObjectToPosition(this.camera, 2, this.tweenDuration * 0.75);
-		this.moveObjectToPosition(this.player, 2.5, this.tweenDuration, () => {});
+		this.movementEventBus.publish("player-side-moved")
+		this.rotateObject(this.player, Math.PI * 0.75)
+		this.moveObjectToPosition(this.camera, 2, this.tweenDuration * 0.75)
+		this.moveObjectToPosition(this.player, 2.5, this.tweenDuration, () => {})
 	}
 
 	moveToLane(lane) {
 		if (lane === this.currentLane) {
-			return;
+			return
 		} else if (
 			(lane === "left" && this.currentLane === "right") ||
 			(lane === "right" && this.currentLane === "left")
 		) {
-			this.prevLane = this.currentLane;
-			this.currentLane = LANES.CENTER;
-			this.updateCharacterPosition();
-			return;
+			this.prevLane = this.currentLane
+			this.currentLane = LANES.CENTER
+			this.updateCharacterPosition()
+			return
 		}
-		this.currentLane = lane;
-		this.updateCharacterPosition();
+		this.currentLane = lane
+		this.updateCharacterPosition()
 	}
 
 	updateCharacterPosition() {
 		switch (this.currentLane) {
 			case "left":
-				this.moveLeft();
-				break;
+				this.moveLeft()
+				break
 			case "center":
-				this.moveToCenter();
-				break;
+				this.moveToCenter()
+				break
 			case "right":
-				this.moveRight();
-				break;
+				this.moveRight()
+				break
 			default:
-				break;
+				break
 		}
 	}
 
 	slide() {
-		this.movementEventBus.publish("player-slided");
-		this.isSliding = true;
-		this.context.G.PLAYER_SLIDING = this.isSliding;
-		setTimeout(() => {
-			this.player.position.y = 0;
-			this.isSliding = false;
-			this.context.G.PLAYER_SLIDING = this.isSliding;
-		}, 1600);
+		if (this.isJumping) return
+		this.movementEventBus.publish("player-slided")
+		this.isSliding = true
+		this.context.G.PLAYER_SLIDING = this.isSliding
+		setTimeout(() => {}, 1600)
 
 		gsap.to(this.context.gameWorld.camera.position, {
 			y: 0.6,
 			z: 2.4,
 			duration: 0.8,
 
-			onComplete: () =>
+			onComplete: () => {
 				gsap.to(this.context.gameWorld.camera.position, {
 					y: 2.2847,
 					z: 5.582,
 					duration: 0.8,
-				}),
-		});
+
+					onComplete: () => {
+						this.player.position.y = 0
+						this.isSliding = false
+						this.context.G.PLAYER_SLIDING = this.isSliding
+					},
+				})
+			},
+		})
 	}
 
 	jump() {
-		this.movementEventBus.publish("player-jumped");
-		this.canJump = true;
-		this.context.G.PLAYER_JUMPING = this.canJump;
+		if (this.isSliding) return
+		this.movementEventBus.publish("player-jumped")
+		this.isJumping = true
+		this.context.G.PLAYER_JUMPING = this.isJumping
 
 		gsap.to(this.context.gameWorld.camera.position, {
 			y: 4,
@@ -150,7 +155,7 @@ export default class MovementManager {
 					z: 5.582,
 					duration: 0.6,
 				}),
-		});
+		})
 
 		gsap.to(this.player.position, {
 			y: 3,
@@ -160,15 +165,16 @@ export default class MovementManager {
 			onComplete: () =>
 				gsap.to(this.player.position, {
 					y: 0,
-					z: 1.5,
+					z: 1,
 					duration: 0.6,
+					onComplete: () => {
+						this.isJumping = false
+						this.context.G.PLAYER_JUMPING = this.isJumping
+					},
 				}),
-		});
+		})
 
-		setTimeout(() => {
-			this.canJump = false;
-			this.context.G.PLAYER_JUMPING = this.canJump;
-		}, 1810);
+		// setTimeout(() => {}, 1810)
 	}
 
 	update(delta) {}
